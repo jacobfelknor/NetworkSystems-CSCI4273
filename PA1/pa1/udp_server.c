6a803319d420fb41fe5eeee7a75e5a2ef45d52c1
Jacob Felknor <jacobfelknor073@gmail.com>
2022-01-23 20:53:22 -0700

    ls and exit working

--- PA1/pa1/udp_server.c
+++ PA1/pa1/udp_server.c
@@ -8,37 +8,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <linux/limits.h>
 
 #define BUFSIZE 1024
 
 /*
  * error - wrapper for perror
  */
-void error(char *msg) {
+void error(char *msg)
+{
   perror(msg);
   exit(1);
 }
 
-int main(int argc, char **argv) {
-  int sockfd; /* socket */
-  int portno; /* port to listen on */
-  int clientlen; /* byte size of client's address */
+void putFileInBuffer(char *buf, FILE *f)
+{
+  // clear buffer first
+  bzero(buf, BUFSIZE);
+  fseek(f, 0, SEEK_END);
+  long fsize = ftell(f);
+  fseek(f, 0, SEEK_SET); /* same as rewind(f); */
+
+  // char *string = malloc(fsize + 1);
+  fread(buf, fsize, 1, f);
+  fclose(f);
+
+  // string[fsize] = 0;
+}
+
+int main(int argc, char **argv)
+{
+  int sockfd;                    /* socket */
+  int portno;                    /* port to listen on */
+  int clientlen;                 /* byte size of client's address */
   struct sockaddr_in serveraddr; /* server's addr */
   struct sockaddr_in clientaddr; /* client addr */
-  struct hostent *hostp; /* client host info */
-  char buf[BUFSIZE]; /* message buf */
-  char *hostaddrp; /* dotted decimal host addr string */
-  int optval; /* flag value for setsockopt */
-  int n; /* message byte size */
+  struct hostent *hostp;         /* client host info */
+  char buf[BUFSIZE];             /* message buf */
+  char *hostaddrp;               /* dotted decimal host addr string */
+  int optval;                    /* flag value for setsockopt */
+  int n;                         /* message byte size */
 
   /* 
    * check command line arguments 
    */
-  if (argc != 2) {
+  if (argc != 2)
+  {
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
     exit(1);
   }
@@ -48,7 +67,7 @@ int main(int argc, char **argv) {
    * socket: create the parent socket 
    */
   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sockfd < 0) 
+  if (sockfd < 0)
     error("ERROR opening socket");
 
   /* setsockopt: Handy debugging trick that lets 
@@ -57,13 +76,13 @@ int main(int argc, char **argv) {
    * Eliminates "ERROR on binding: Address already in use" error. 
    */
   optval = 1;
-  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
-	     (const void *)&optval , sizeof(int));
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+             (const void *)&optval, sizeof(int));
 
   /*
    * build the server's Internet address
    */
-  bzero((char *) &serveraddr, sizeof(serveraddr));
+  bzero((char *)&serveraddr, sizeof(serveraddr));
   serveraddr.sin_family = AF_INET;
   serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
   serveraddr.sin_port = htons((unsigned short)portno);
@@ -71,45 +90,72 @@ int main(int argc, char **argv) {
   /* 
    * bind: associate the parent socket with a port 
    */
-  if (bind(sockfd, (struct sockaddr *) &serveraddr, 
-	   sizeof(serveraddr)) < 0) 
+  if (bind(sockfd, (struct sockaddr *)&serveraddr,
+           sizeof(serveraddr)) < 0)
     error("ERROR on binding");
 
   /* 
    * main loop: wait for a datagram, then echo it
    */
   clientlen = sizeof(clientaddr);
-  while (1) {
+  while (1)
+  {
 
     /*
      * recvfrom: receive a UDP datagram from a client
      */
     bzero(buf, BUFSIZE);
     n = recvfrom(sockfd, buf, BUFSIZE, 0,
-		 (struct sockaddr *) &clientaddr, &clientlen);
+                 (struct sockaddr *)&clientaddr, &clientlen);
     if (n < 0)
       error("ERROR in recvfrom");
 
     /* 
      * gethostbyaddr: determine who sent the datagram
      */
-    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+                          sizeof(clientaddr.sin_addr.s_addr), AF_INET);
     if (hostp == NULL)
       error("ERROR on gethostbyaddr");
     hostaddrp = inet_ntoa(clientaddr.sin_addr);
     if (hostaddrp == NULL)
       error("ERROR on inet_ntoa\n");
-    printf("server received datagram from %s (%s)\n", 
-	   hostp->h_name, hostaddrp);
+    printf("server received datagram from %s (%s)\n",
+           hostp->h_name, hostaddrp);
     printf("server received %d/%d bytes: %s\n", strlen(buf), n, buf);
-    
+
+    // Check command that was sent. Respond accordingly
+    if (strcmp(buf, "exit\n") == 0)
+    {
+      bzero(buf, BUFSIZE);
+      strncpy(buf, "Goodbye", sizeof buf - 1);
+    }
+    else if (strcmp(buf, "ls\n") == 0)
+    {
+      FILE *fp;
+      int status;
+      char path[PATH_MAX];
+
+      fp = popen("ls", "r");
+      if (fp == NULL)
+        /* Handle error */;
+      // clear buffer first, then store contents of ls
+      putFileInBuffer(buf, fp);
+      // while (fgets(path, PATH_MAX, fp) != NULL)
+      // {
+      //   printf("%s", path);
+      // }
+    }
+
     /* 
      * sendto: echo the input back to the client 
      */
-    n = sendto(sockfd, buf, strlen(buf), 0, 
-	       (struct sockaddr *) &clientaddr, clientlen);
-    if (n < 0) 
+    int len;
+    len = strlen(buf);
+
+    n = sendto(sockfd, buf, strlen(buf), 0,
+               (struct sockaddr *)&clientaddr, clientlen);
+    if (n < 0)
       error("ERROR in sendto");
   }
 }
